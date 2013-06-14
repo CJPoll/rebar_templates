f5c242aec5742b0a407a195b3eaae438db466e6e
Cody Poll <CJPoll@gmail.com>
2013-06-13 22:31:23 -0600

    Supervisor template

---
+++ supervisor.erl
@@ -0,0 +1,22 @@
+-module({{name}}).
+-behavior(supervisor).
+
+%%% Callback Exports
+-export([init/1]).
+
+%%% API Exports
+-export([start_link/0]).
+
+start_link() ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init(_Args) ->
+	Child = {{child}},
+	Restart = {{restart}},
+	ChildSpec = {Child,
+		{Child, start_link, []},
+		permanent, 5000, worker, [Child]},
+
+	{ok, {
+			{Restart, 5, 10},
+			[ChildSpec]}}.
